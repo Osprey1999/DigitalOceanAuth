b13ba7380338dbd98c385ba90eb27c5adff738d7
Osprey1999 <hiabheek@gmail.com>
2023-09-24 20:54:11 +0530

    chore: modified http responses
    Co-authored-by: Osprey1999 <hiabheek@gmail.com>

--- packages/auth/signUp/signUp.js
+++ packages/auth/signUp/signUp.js
@@ -4,47 +4,49 @@ const bcrypt = require("bcrypt");
 
 module.exports.main =  async (event) => {
   const {http,...body} = event
-  res = {
-      status: 200,
-      error: false, 
-      message: "success"
+  try {
+    const { error } = signUpBodyValidation(body);
+    if (error){
+        res = {
+          statusCode: 400,
+          body:{
+            error: true, 
+            message: error.details[0].message
+          }
+        }
+      return res
+    }
+    const user = await User.findOne({ email: body.email });
+    if (user){
+      res = {
+        statusCode: 400,
+        body:{
+          error: true, 
+          message: "User with given email already exist"
+        }
+      }
+      return res
+    }
+
+    const salt = await bcrypt.genSalt(Number(process.env.SALT));
+    const hashPassword = await bcrypt.hash(body.password, salt);
+
+    await User.create({ ...body, password: hashPassword });
+    return res = {
+      statusCode: 200,
+      body:{
+        error: false, 
+        message: "Account Created successfully"
+      }
+    }
+  } catch (err) {
+      console.log("Internal Server Error");
+      return res = {
+        statusCode: 400,
+        body:{
+          error: true, 
+          message: "Internal Server Error"
+        }
+      }
   }
-  return res
-  // try {
-  //   const { error } = signUpBodyValidation(body);
-  //   if (error){
-  //       res = {
-  //         status: 400,
-  //         error: true, 
-  //         message: error.details[0].message
-  //     }
-  //     return res
-  //   }
-  //   const user = await User.findOne({ email: body.email });
-  //   if (user){
-  //     res = {
-  //     status: 400,
-  //     error: true, 
-  //     message: "User with given email already exist"
-  //     }
-  //     return res
-  //   }
-
-  //   const salt = await bcrypt.genSalt(Number(process.env.SALT));
-  //   const hashPassword = await bcrypt.hash(body.password, salt);
-
-  //   await User.create({ ...body, password: hashPassword });
-  //   return res = {
-  //     status: 200,
-  //     error: false, 
-  //     message: "Account Created successfully"
-  //   }
-  // } catch (err) {
-  //     console.log("Internal Server Error");
-  //     res = {
-  //       status: 500,
-  //       error: true, 
-  //       message: "Internal Server Error"
-  //     }
-  // }
 }
